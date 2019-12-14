0df4019cb413a0f151fe1ce634bdebb67f0e2487
Wellington Bernardes <webean@outlook.com>
2019-12-14 09:35:42 -0200

    Imprimindo "usuario"

--- aula004.js
+++ aula004.js
@@ -1,3 +1,6 @@
-// Usuários
+// Usuários (Letra "u" diferente nas variáveis)
 var nomeusuario = "João";
 var nomeUsuario = "Tiago";
+
+console.log("nomeusuario = " + nomeusuario);
+// Vai imprimir João
